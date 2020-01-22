e43b7eb580e1e48cdb422e0e907f2371d46b27af
Matt <stepitm@wwu.edu>
2020-01-22 14:03:35 -0800

    separated out game of life

---
+++ ConsoleApp1/GameOfLife.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using DataStructures;
+public class GameOfLife
+{
+
+	ArrayList<ArrayList<int>> prevmatrix;
+
+    int horzSize;
+    int vertSize;
+    int deltaTime;
+	public GameOfLife(int horz, int vert, int deltaTime)
+	{
+        this.horzSize = horz;
+        this.vertSize = vert;
+        this.deltaTime = deltaTime;
+
+		prevmatrix = new ArrayList<ArrayList<int>>(true);
+        //initilize matrix
+        for (int i = 0; i < vertSize; ++i)
+        {
+            ArrayList<int> addition = new ArrayList<int>();
+            for (int j = 0; j < horzSize; ++j)
+            {
+                if (i > 4 && j > 5 && j < 10)
+                {
+                    addition.Append(255);
+                }
+                else
+                {
+                    addition.Append(0);
+                }
+
+            }
+            prevmatrix.Append(addition);
+
+        }
+
+    }
+
+    public ArrayList<ArrayList<int>> StepGame()
+    {
+        ArrayList<ArrayList<int>> matrix = new ArrayList<ArrayList<int>>(true);
+
+        for (int i = 0; i < vertSize; ++i)
+        {
+            ArrayList<int> addition = new ArrayList<int>();
+            for (int j = 0; j < horzSize; ++j)
+            {
+                int avr = 0;
+
+
+                if (i - 1 > 0)
+                {
+                    avr += prevmatrix.Index(i - 1).Index(j);
+                }
+                if (j + 1 < horzSize)
+                {
+                    avr += prevmatrix.Index(i).Index(j + 1);
+                }
+                if (i + 1 < vertSize)
+                {
+                    avr += prevmatrix.Index(i + 1).Index(j);
+                }
+                if (j - 1 > 0)
+                {
+                    avr += prevmatrix.Index(i).Index(j - 1);
+                }
+
+
+                if (i - 1 > 0 && j + 1 < horzSize)
+                {
+                    avr += prevmatrix.Index(i - 1).Index(j + 1);
+                }
+                if (i + 1 < vertSize && j + 1 < horzSize)
+                {
+                    avr += prevmatrix.Index(i + 1).Index(j + 1);
+                }
+                if (i + 1 < vertSize && j - 1 > 0)
+                {
+                    avr += prevmatrix.Index(i + 1).Index(j - 1);
+                }
+                if (i - 1 > 0 && j - 1 > 0)
+                {
+                    avr += prevmatrix.Index(i - 1).Index(j - 1);
+                }
+
+
+
+
+
+                if (prevmatrix.Index(i).Index(j) > 127) //alive
+                {
+                    if (avr >= 255 + 255 && avr <= 255 + 255 + 255) //two or three
+                    {
+                        addition.Append(255);
+                    }
+                    else
+                    {
+                        addition.Append(0); //too few or too many so die
+                    }
+                }
+                else //dead
+                {
+                    if (avr >= 255 + 255 + 255)//come back to life
+                    {
+                        addition.Append(255);
+                    }
+                    else
+                    {
+                        addition.Append(prevmatrix.Index(i).Index(j) - prevmatrix.Index(i).Index(j) / 2);
+                    }
+                }
+
+
+
+
+
+            }
+            matrix.Append(addition);
+
+        }
+
+        
+        prevmatrix = matrix;
+
+        System.Threading.Thread.Sleep(deltaTime);
+
+        return prevmatrix;
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using DataStructures;
+
 namespace ConsoleApp1
 {
     class Program
@@ -8,129 +9,18 @@ namespace ConsoleApp1
         {
 
 
-
-
-
-            int horzSize = 30;
+            int horzSize = 60;
             int vertSize = 30;
 
-            ArrayList<ArrayList<int>> prevmatrix = new ArrayList<ArrayList<int>>(true);
-            //init
-
-            for (int i = 0; i < vertSize; ++i)
-            {
-                ArrayList<int> addition = new ArrayList<int>();
-                for (int j = 0; j < horzSize; ++j)
-                {
-                    if(i > 4 && j > 5 && j < 10)
-                    {
-                        addition.Append(255);
-                    }
-                    else
-                    {
-                        addition.Append(0);
-                    }
-                    
-                }
-                prevmatrix.Append(addition);
-                
-            }
+        
+            GameOfLife gol = new GameOfLife(horzSize, vertSize,300);
 
             AsciiConverter ac = new AsciiConverter(horzSize, vertSize);
 
-            int iteration = 0;
+            //int iteration = 0;
             while (true)
             {
-                ArrayList<ArrayList<int>> matrix = new ArrayList<ArrayList<int>>(true);
-
-                for (int i = 0; i < vertSize; ++i)
-                {
-                    ArrayList<int> addition = new ArrayList<int>();
-                    for (int j = 0; j < horzSize; ++j)
-                    {
-                        int avr = 0;
-
-
-                        if(i-1 > 0)
-                        {
-                            avr += prevmatrix.Index(i - 1).Index(j);
-                        }
-                        if(j+1 < horzSize)
-                        {
-                            avr += prevmatrix.Index(i).Index(j + 1);
-                        }
-                        if(i+1 < vertSize)
-                        {
-                            avr += prevmatrix.Index(i + 1).Index(j);
-                        }
-                        if(j-1 > 0)
-                        {
-                            avr += prevmatrix.Index(i).Index(j - 1);
-                        }
-
-
-                        if (i - 1 > 0 && j+1 <horzSize)
-                        {
-                            avr += prevmatrix.Index(i - 1).Index(j+1);
-                        }
-                        if (i + 1 < vertSize && j + 1 < horzSize  )
-                        {
-                            avr += prevmatrix.Index(i+1).Index(j + 1);
-                        }
-                        if (i + 1 < vertSize && j -1  >0)
-                        {
-                            avr += prevmatrix.Index(i + 1).Index(j-1);
-                        }
-                        if (i-1 > 0 && j - 1 > 0)
-                        {
-                            avr += prevmatrix.Index(i-1).Index(j - 1);
-                        }
-
-
-
-
-
-                        if (prevmatrix.Index(i).Index(j) >127) //alive
-                        {
-                            if(avr >= 255 +255 && avr <= 255 + 255 +255) //two or three
-                            {
-                                addition.Append(255);
-                            }
-                            else
-                            {
-                                addition.Append(0); //too few or too many so die
-                            }
-                        }
-                        else //dead
-                        {
-                            if(avr >= 255 + 255 + 255)//come back to life
-                            {
-                                addition.Append(255);
-                            }
-                            else
-                            {
-                                addition.Append(prevmatrix.Index(i).Index(j) - prevmatrix.Index(i).Index(j)/2);
-                            }
-                        }
-                        
-                        
-
-
-
-                    }
-                    matrix.Append(addition);
-                    
-                }
-                
-                
-                Console.Write(ac.AsciiToString(prevmatrix));
-                prevmatrix = matrix;
-
-                System.Threading.Thread.Sleep(300);
-                //Console.Read();
-
-
-                ++iteration;
+                Console.WriteLine(ac.AsciiToString(gol.StepGame()));
             }
            
 
